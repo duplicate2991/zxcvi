e6fb752e2f5b948242191e788d2f2a982ee568ee
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-09-19 17:28:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const items = ['Item 1', 'Item 2', 'Item 3'];
+
 var router = require("./router.js");
+app.set('view engine', 'ejs');
 app.use("/", router);
 // Set EJS as the view engine
-app.set('view engine', 'ejs');
+
 
 // Define a route to render the index.ejs template
 app.get('/ejs', (req, res) => {
@@ -18,3 +21,27 @@ app.listen(port, () => {
 
 
 module.exports = app
+
+
+/*
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Sample data (items)
+const items = ['Item 1', 'Item 2', 'Item 3'];
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Define a route to render the index.ejs template
+app.get('/', (req, res) => {
+    res.render('index', { items });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+    */
