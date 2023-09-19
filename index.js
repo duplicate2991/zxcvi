abb68fd7ca9413760ac2d3b1a7cf2fee2c63c009
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-09-19 17:21:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 const port = 3000;
 var router = require("./router.js");
 app.use("/", router);
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Define a route to render the index.ejs template
+app.get('/ejs', (req, res) => {
+    res.render('index', { items });
+});
 
 // Start the server
 app.listen(port, () => {
