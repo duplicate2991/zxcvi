e3095d34c4f199adb5ead3bf2fea4c820f2ba616
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-09-27 09:36:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 var { Client, GatewayIntentBits } = require('discord.js');
+function ms(milliseconds){
+    const roundTowardsZero = milliseconds > 0 ? Math.floor : Math.ceil;
+	return {
+		days: roundTowardsZero(milliseconds / 86400000),
+		hours: roundTowardsZero(milliseconds / 3600000) % 24,
+		minutes: roundTowardsZero(milliseconds / 60000) % 60,
+		seconds: roundTowardsZero(milliseconds / 1000) % 60,
+		milliseconds: roundTowardsZero(milliseconds) % 1000,
+		microseconds: roundTowardsZero(milliseconds * 1000) % 1000,
+		nanoseconds: roundTowardsZero(milliseconds * 1e6) % 1000
+	};
+}
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
@@ -10,6 +22,11 @@ client.on('interactionCreate', async interaction => {
 
   if (interaction.commandName === 'ping') {
     await interaction.reply('Pong!');
+  }
+});
+client.on("messageCreate", async(msg) => {
+if(msg.content.startsWith(".uptime")){
+    message.reply({content: `${ms(client.uptime).days}:${ms(client.uptime).hours}:${ms(client.uptime).minutes}:${ms(client.uptime).seconds}:`});
   }
 });
 
